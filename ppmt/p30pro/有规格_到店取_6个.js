3ff01338ee411be7fcd5a599e8c09dc11cc9b07f
morphone <179464330@qq.com>
2025-05-05 21:04:11 +0800

    feat:1

--- ppmt/p30pro/有规格_到店取_6个.js
+++ ppmt/p30pro/有规格_到店取_6个.js
@@ -53,7 +53,6 @@ const state = {
   enterSureLoop: false,
   loopCount: 0,
 };
-
 let main = () => {
   requestScreenCapture();
   startToBuy(); // 寻找立即购买按钮
@@ -75,6 +74,7 @@ let main = () => {
           quickBuyScreen,
           chooseDetailScreen,
           markListScreen,
+          makeSureOrderScreen,
         } = patchScreen(currentScreenOcr);
 
         console.log(
@@ -83,14 +83,19 @@ let main = () => {
           currentScreenOcr,
           hasQuickBuyBtn,
           quickBuyScreen,
-          chooseDetailScreen
+          chooseDetailScreen,
+          makeSureOrderScreen
         );
         // 进入选择规格/购买方式页面  break
         if (chooseDetailScreen) {
-          console.log(
-            "存在购买方式文字，说明已经进入购买页面，开始初始化购买配置，并打断当前子线程"
-          );
+          console.log("进入购买页面，开始初始化购买配置，并打断当前子线程");
           initBuyMethod(); //初始化购买配置页面
+          break;
+        }
+        // 确认信息页面
+        if (makeSureOrderScreen) {
+          console.log("开始确认订单循环，并打断当前子线程");
+          handleToPayLoop();
           break;
         }
         // 误点进入自提门店列表页面 马上退出F
