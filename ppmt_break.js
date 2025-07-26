bbb16d02dac5b1b6b74dbf613451867a57648787
morphone <179464330@qq.com>
2025-07-26 23:57:50 +0800

    1

--- ppmt_break.js
+++ ppmt_break.js
@@ -2,10 +2,16 @@ auto();
 let state = {
   loopBuyMethodTime: 3000,
   loopBuyMethodCount: 0, // 循环点击送到家 到店取次数
-  loopBuyMethodStatus: false, // 循环点击送到家 到店取状态机 暂时未使用
+  loopBuyMethodPatchThread: null,
+  // loopBuyMethodStatus: false, // 循环点击送到家 到店取状态机 暂时未使用
+  loopPlaceOrderTime: 3000,
+  loopPlaceOrderCount: 0,
+  loopPlaceOrderStep: "", // sureAndPay sureInfo orderResult rebackBuyMethodPage
+  // sonThreadMap: {}, // 暂时未使用
+  loopPlaceOrderPatchThread: null,
   widghtFindTime: 3000, //查找widght的最大时间
   hasStandard: true, //是否有选择规格
-  refreshWithoutFeel: true, // 是否无感刷新
+  refreshWithoutFeel: false, // 是否无感刷新
   buyMethod: "mark", // home | mark  // 选择的购买方式
   currentMethod: "", // 当前的购买方式
   addOne: true, //是否数量+1
@@ -17,10 +23,13 @@ const eventKeys = {
   start: "start",
   swipe: "swipe",
 };
-
 const introductionPage = "introduction";
 const buyMethodPage = "buyMethod";
 const placeOrderPage = "placeOrder";
+const sureAndPay = "sureAndPay";
+const sureInfo = "sureInfo";
+const orderResult = "orderResult";
+const rebackBuyMethodPage = "rebackBuyMethodPage";
 
 if (!global.javaTimer) {
   global.javaTimer = new java.util.Timer(true);
@@ -33,24 +42,29 @@ events.on("exit", () => {
   }
 });
 function javaSetTimeout(callback, delay) {
-  const timerTask = new java.util.TimerTask({
-    run: () => {
-      try {
-        // 自动处理 UI 线程操作
-        if (typeof ui !== "undefined") {
-          ui.run(callback);
-        } else {
-          callback();
-        }
-      } catch (e) {
-        console.error("定时任务执行出错: " + e);
-      }
-    },
-  });
-
   // 正确的调度方法 - 使用两个参数的重载
-  global.javaTimer.schedule(timerTask, delay);
-  return timerTask;
+  if (global.javaTimer) {
+    const timerTask = new java.util.TimerTask({
+      run: () => {
+        try {
+          // 自动处理 UI 线程操作
+          if (typeof ui !== "undefined") {
+            ui.run(callback);
+          } else {
+            callback();
+          }
+        } catch (e) {
+          console.error("定时任务执行出错: " + e);
+        }
+      },
+    });
+    global.javaTimer.schedule(timerTask, delay);
+    return timerTask;
+  } else {
+    return setTimeout(() => {
+      callback && callback();
+    }, delay);
+  }
 }
 
 function javaClearTimeout(timerTask) {
@@ -70,43 +84,6 @@ function javaClearTimeout(timerTask) {
 // javaClearTimeout(myTask);
 
 const event$ = events.emitter();
-
-// 商品开始页面  页面一
-function handlerIntroductionThread() {
-  event$.on(eventKeys.start, ({ mode, from }) => {
-    if (from !== introductionPage) return;
-    console.log({ from }, "页面一");
-    if (mode === "start") {
-      handleQuickBuyClick({
-        fn: () => {
-          state.currentPage = buyMethodPage;
-          event$.emit(eventKeys.jump, {
-            mode: "init",
-            from: introductionPage,
-          });
-        },
-      });
-    }
-  });
-}
-
-// 选择购买方式页面  页面二
-function handlerBuyMethodThread() {
-  event$.on(eventKeys.start, ({ mode, from }) => {
-    if (from !== buyMethodPage) return;
-    console.log({ from }, "页面二");
-    initBuyMethod();
-    loopBuyMethod();
-  });
-  event$.on(eventKeys.jump, ({ mode, from }) => {
-    if (mode === "init" && from === introductionPage) {
-      console.log({ from }, "页面二");
-      initBuyMethod();
-      loopBuyMethod();
-    }
-  });
-  // watchSwipe();
-}
 
 // 初始化购买详情页面配置
 function initBuyMethod() {
@@ -131,9 +108,7 @@ function initBuyMethod() {
   patchPageFeature({
     text: "购买方式",
     callback: () => {
-      let sureBtnSync = className("android.widget.TextView")
-        .text("确定")
-        .exists();
+      let sureBtnSync = checkTextViewWidgetIsExists("确定");
       console.log(
         "进入页面第一次选择购买方式,先判断确定按钮是否已存在",
         sureBtnSync
@@ -176,11 +151,14 @@ function initBuyMethod() {
       });
     },
   });
-  // 确定按钮 异步
-  patchPageFeature({
-    text: "确定",
-    callback: handleSureClick,
-  });
+  // // // 确定按钮 异步
+  // if (state.loopBuyMethodPatchThread) {
+  //   state.loopBuyMethodPatchThread.interrupt();
+  // }
+  // state.loopBuyMethodPatchThread = patchPageFeature({
+  //   text: "确定",
+  //   callback: handleSureClick,
+  // });
 }
 
 // 循环购买方式
@@ -224,11 +202,14 @@ function patchBuyMethodPageBtnStatus() {
     }
   }
   const btns = ["loading", "已售罄", "去授权", "选择门店", "确定"];
-  startThread({
+  if (state.loopBuyMethodPatchThread) {
+    state.loopBuyMethodPatchThread.interrupt();
+  }
+  state.loopBuyMethodPatchThread = startThread({
     fn: () => {
       while (runThread) {
         let features = btns.map((b, index) => {
-          let bool = className("android.widget.TextView").text(b).exists();
+          let bool = checkTextViewWidgetIsExists(b);
           if (b === "loading") {
             bool = className("android.widget.Image").depth(24).exists();
           }
@@ -241,22 +222,44 @@ function patchBuyMethodPageBtnStatus() {
       }
     },
   });
-
   javaSetTimeout(() => {
     runThread = false;
   }, state.loopBuyMethodTime);
 }
 
-function handleSimulateClick({ x, y, duration }) {
-  const _duration = duration || 1;
-  // press(x, y, _duration)
-  click(x, y);
-  // click(child.bounds().centerX(), child.bounds().centerY());
+function handleSimulateClick({
+  widget,
+  mockClick = false,
+  callback,
+  widgetKey,
+  sleepTime = 0,
+} = {}) {
+  if (widget) {
+    if (widgetKey) {
+      state[widgetKey] = {
+        x: widget.bounds().centerX(),
+        y: widget.bounds().centerY(),
+      };
+    }
+    if (mockClick) {
+      const x = state[widgetKey].x;
+      const y = state[widgetKey].y;
+      if (x && y) {
+        click(x, y);
+      } else {
+        widget.click();
+      }
+    } else {
+      widget.click();
+    }
+    sleep(sleepTime);
+    callback && callback();
+  }
 }
 
 // 点击确定按钮
 function handleSureClick() {
-  if (state.loopBuyMethodCount < 5) return;
+  // if (state.loopBuyMethodCount < 4) return;
   console.log("已找到确定按钮确定");
   let sureBtn = findTextViewWidget({ text: "确定" });
   if (!sureBtn) return;
@@ -272,25 +275,10 @@ function handleSureClick() {
   }
   sureBtn.click();
   state.currentPage = placeOrderPage;
+  state.loopPlaceOrderStep = sureAndPay;
   event$.emit(eventKeys.jump, {
     mode: "init",
     from: buyMethodPage,
-  });
-}
-
-// 确认信息并支付页面  页面三
-function handlerPlaceOrderThread() {
-  event$.on(eventKeys.start, ({ mode, from }) => {
-    if (from !== placeOrderPage) return;
-    console.log(from);
-  });
-
-  event$.on(eventKeys.jump, ({ mode, from }) => {
-    if (mode === "init" && from === buyMethodPage) {
-      console.log({ from }, "页面三");
-      // initBuyMethod();
-      // loopBuyMethod();
-    }
   });
 }
 
@@ -312,9 +300,7 @@ function eventTimeControl({ fn, time = 0, endFn }) {
 
 // 创建子线程
 function startThread({ threadKey, fn }) {
-  let t = threads.start(function () {
-    fn();
-  });
+  let t = threads.start(fn);
   threadKey && setInterval(() => {}, 1000);
   t.waitFor();
   return t;
@@ -326,12 +312,10 @@ function patchPageFeature({ callback, text, timeOut, sync }) {
     callback();
   } else {
     let runThread = true;
-    startThread({
+    let t = startThread({
       fn: () => {
         while (runThread) {
-          let feature = className("android.widget.TextView")
-            .text(text)
-            .exists();
+          let feature = checkTextViewWidgetIsExists(text);
           if (feature) {
             callback && callback();
             break;
@@ -342,6 +326,7 @@ function patchPageFeature({ callback, text, timeOut, sync }) {
     javaSetTimeout(() => {
       runThread = false;
     }, timeOut || state.widghtFindTime);
+    return t;
   }
 }
 
@@ -364,28 +349,34 @@ function findTextViewWidget({ text }) {
     .findOne(state.widghtFindTime);
 }
 
+function checkTextViewWidgetIsExists(text) {
+  return className("android.widget.TextView").text(text).exists();
+}
+
 // 匹配当前是哪个页面
 function patchPage({ callback }) {
   let runThread = true;
   startThread({
     fn: () => {
       while (runThread) {
-        let feature0 = className("android.widget.TextView")
-          .text("购物车")
-          .exists();
-        let feature1 = className("android.widget.TextView")
-          .text("购买方式")
-          .exists();
-        let feature2 = className("android.widget.TextView")
-          .text("确认信息并支付")
-          .exists();
+        let feature0 =
+          checkTextViewWidgetIsExists("购物车") ||
+          checkTextViewWidgetIsExists("立即购买");
+        let feature1 = checkTextViewWidgetIsExists("购买方式");
+        let feature2 = checkTextViewWidgetIsExists("确认信息并支付");
         console.log({ feature0, feature1, feature2 });
-        if (feature0 && feature1 && feature2) {
+        if (
+          (feature0 && feature1 && feature2) ||
+          (!feature0 && !feature1 && feature2)
+        ) {
           state.currentPage = placeOrderPage;
           callback({ page: placeOrderPage });
           break;
         }
-        if (feature0 && feature1 && !feature2) {
+        if (
+          (feature0 && feature1 && !feature2) ||
+          (!feature0 && feature1 && !feature2)
+        ) {
           state.currentPage = buyMethodPage;
           callback({ page: buyMethodPage });
           break;
@@ -395,12 +386,16 @@ function patchPage({ callback }) {
           callback({ page: introductionPage });
           break;
         }
+        if (!feature0 && !feature1 && !feature2) {
+          // TODO 无法获取控件
+          // break;
+        }
       }
     },
   });
   javaSetTimeout(() => {
     runThread = false;
-  }, state.widghtFindTime);
+  }, 24 * 60 * 60);
 }
 
 function watchSwipe() {
@@ -419,6 +414,214 @@ function watchSwipe() {
   });
 }
 
+// 商品开始页面  页面一
+function handlerIntroductionThread() {
+  event$.on(eventKeys.start, ({ mode, from }) => {
+    if (from !== introductionPage) return;
+    console.log({ from }, "页面一");
+    if (mode === "start") {
+      handleQuickBuyClick({
+        fn: () => {
+          state.currentPage = buyMethodPage;
+          event$.emit(eventKeys.jump, {
+            mode: "init",
+            from: introductionPage,
+          });
+        },
+      });
+    }
+  });
+}
+
+// 选择购买方式页面  页面二
+function handlerBuyMethodThread() {
+  event$.on(eventKeys.start, ({ mode, from }) => {
+    if (from !== buyMethodPage) return;
+    console.log({ from }, "页面二");
+    initBuyMethod();
+    loopBuyMethod();
+  });
+  event$.on(eventKeys.jump, ({ mode, from }) => {
+    if (mode === "init" && from === introductionPage) {
+      console.log({ from }, "页面二");
+      initBuyMethod();
+      loopBuyMethod();
+    }
+  });
+  // watchSwipe();
+}
+
+// 确认信息并支付页面  页面三
+function handlerPlaceOrderThread() {
+  event$.on(eventKeys.start, ({ mode, from }) => {
+    if (from !== placeOrderPage) return;
+    console.log({ from }, "页面三start");
+    patchPlaceOrderFeature({
+      callback: ({ currentStep }) => {
+        log(222);
+        state.loopPlaceOrderStep = currentStep;
+        loopPlaceOrder();
+      },
+      static: true,
+    });
+  });
+
+  event$.on(eventKeys.jump, ({ mode, from }) => {
+    if (mode === "init" && from === buyMethodPage) {
+      console.log({ from }, "页面三jump");
+      loopPlaceOrder();
+    }
+  });
+}
+
+// 下单轮询
+function loopPlaceOrder() {
+  patchPlaceOrderFeature({
+    callback: ({ currentStep, errorInfo }) => {
+      let stepMap = {
+        sureAndPay: {
+          textFeature: "确认信息并支付",
+          nextStep: sureInfo,
+        },
+        sureInfo: {
+          textFeature: state.buyMethod === "home" ? "确认无误" : "就是这家",
+          nextStep: orderResult,
+        },
+        orderResult: {
+          textFeature: "我知道了",
+          nextStep: rebackBuyMethodPage,
+        },
+        rebackBuyMethodPage: {
+          textFeature: "确定",
+          nextStep: sureAndPay,
+        },
+      };
+      handleSimulateClick({
+        widget: findTextViewWidget({
+          text: stepMap[currentStep].textFeature,
+        }),
+        callback: () => {
+          state.loopPlaceOrderStep = stepMap[currentStep].nextStep;
+          loopPlaceOrder();
+        },
+      });
+    },
+  });
+}
+
+function patchPlaceOrderFeature({ callback, static = false } = {}) {
+  let runThread = true;
+  let startTime = Date.now();
+  if (state.loopPlaceOrderPatchThread) {
+    state.loopPlaceOrderPatchThread.interrupt();
+  }
+  state.loopPlaceOrderPatchThread = startThread({
+    fn: () => {
+      while (runThread) {
+        let buyMethodFeature = checkTextViewWidgetIsExists("购买方式");
+        let sureAndPayFeature = checkTextViewWidgetIsExists("确认信息并支付");
+        let sureMailFeature =
+          checkTextViewWidgetIsExists("确认无误") ||
+          checkTextViewWidgetIsExists("请确认收货信息");
+        let sureMarkFeature =
+          checkTextViewWidgetIsExists("请确认以下信息") ||
+          checkTextViewWidgetIsExists("就是这家");
+        let orderResultErrorFeature = checkTextViewWidgetIsExists("我知道了");
+        let orderResultErrorInfoFeature = orderResultErrorFeature
+          ? findTextViewWidget({
+              text: "我知道了",
+            }).previousSibling()
+          : { text: () => "" };
+        let orderResultErrorInfo = orderResultErrorInfoFeature.text(); // 下单失败提示文字
+        // console.log({
+        //   buyMethodFeature,
+        //   sureAndPayFeature,
+        //   sureMailFeature,
+        //   sureMarkFeature,
+        //   orderResultErrorFeature,
+        //   orderResultErrorInfo,
+        //   static,
+        //   loopPlaceOrderStep: state.loopPlaceOrderStep,
+        // });
+
+        // 确认信息 步骤  sureAndPay
+        if (
+          sureAndPayFeature &&
+          !sureMailFeature &&
+          !sureMarkFeature &&
+          !orderResultErrorFeature
+        ) {
+          if (state.loopPlaceOrderStep === sureAndPay || static) {
+            console.log("当前步骤", state.loopPlaceOrderStep);
+            callback && callback({ currentStep: sureAndPay });
+            break;
+          }
+        }
+
+        // 确认邮寄地址/门店地址  步骤 sureInfo
+        if (sureAndPayFeature && (sureMarkFeature || sureMailFeature)) {
+          if (state.loopPlaceOrderStep === sureInfo || static) {
+            console.log("当前步骤", state.loopPlaceOrderStep);
+            sleep(3000);
+            callback && callback({ currentStep: sureInfo });
+            break;
+          }
+        }
+
+        // 下单未成功 返回失败原因步骤 orderResult
+        if (
+          sureAndPayFeature &&
+          !sureMarkFeature &&
+          !sureMailFeature &&
+          orderResultErrorFeature
+        ) {
+          if (state.loopPlaceOrderStep === orderResult || static) {
+            console.log("当前步骤", state.loopPlaceOrderStep);
+            callback &&
+              callback({
+                currentStep: orderResult,
+                errorInfo: orderResultErrorInfo,
+              });
+            break;
+          }
+        }
+
+        // 回退到购买方式页面
+        if (
+          buyMethodFeature &&
+          !sureAndPayFeature &&
+          !sureMailFeature &&
+          !sureMarkFeature &&
+          !orderResultErrorFeature
+        ) {
+          if (state.loopPlaceOrderStep === rebackBuyMethodPage) {
+            console.log("当前步骤", state.loopPlaceOrderStep);
+            callback && callback({ currentStep: rebackBuyMethodPage });
+            break;
+          }
+        }
+
+        if (
+          !sureAndPayFeature &&
+          !sureMailFeature &&
+          !sureMarkFeature &&
+          !orderResultErrorFeature
+        ) {
+          console.log("当前步骤", state.loopPlaceOrderStep);
+          let errorTimeKeep = Date.now() - startTime;
+          if (errorTimeKeep > 3000) {
+            callback && callback({ currentStep: rebackBuyMethodPage });
+            break;
+          }
+        }
+      }
+    },
+  });
+  javaSetTimeout(() => {
+    runThread = false;
+  }, state.widghtFindTime);
+}
+
 function main() {
   initConfig();
   const introductionThread = startThread({
@@ -433,17 +636,17 @@ function main() {
     threadKey: "placeOrderthread",
     fn: handlerPlaceOrderThread,
   });
-  javaSetTimeout(() => {
-    patchPage({
-      callback: ({ page }) => {
-        event$.emit(eventKeys.start, {
-          mode: "start",
-          from: page,
-        });
-      },
-    });
-  }, 20);
-}
+  patchPage({
+    callback: ({ page }) => {
+      event$.emit(eventKeys.start, {
+        mode: "start",
+        from: page,
+      });
+    },
+  });
 
-// watchSwipe();
+  // javaSetTimeout(()=>{
+  //   engines.myEngine().forceStop(); // 强制停止当前脚本
+  // },10000)
+}
 main();
